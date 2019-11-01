ac74ba429489b3b9af83b3b05903353a9939e6ee
ccomrade <d.hryzbil@gmail.com>
2019-11-01 18:52:08 +0100

    Kompletní RTL

--- src/user/rtl.cpp
+++ src/user/rtl.cpp
@@ -1,7 +1,60 @@
+#include <new>
+
 #include "rtl.h"
 
-static thread_local ThreadEnvironment g_threadEnv;
+// data předaná nově vytvořenému vláknu
+struct ThreadEntryData
+{
+	RTL::ThreadMain mainFunc = nullptr;
+	void *param = nullptr;
 
+	// informace o procesu ve kterém je nově vytvořené vlákno spuštěno
+	RTL::ProcessEnvironment process;
+};
+
+
+static thread_local RTL::ThreadEnvironment g_threadEnv;
+
+
+// vstupní bod pro všechna nově vytvořená vlákna kromě hlavního vlákna každého procesu
+static size_t __stdcall ThreadEntry(const kiv_hal::TRegisters & context)
+{
+	RTL::ThreadMain threadMain;
+	void *param;
+
+	{
+		ThreadEntryData *pData = reinterpret_cast<ThreadEntryData*>(context.rdi.r);
+
+		threadMain = pData->mainFunc;
+		param = pData->param;
+
+		// nastavení informací o procesu
+		g_threadEnv.process = pData->process;
+
+		delete pData;
+	}
+
+	// spuštění main funkce nového vlákna
+	int exitCode = threadMain(param);
+
+	RTL::SetCurrentThreadExitCode(exitCode);
+
+	return 0;
+}
+
+// vstupní bod obsluhy signálu
+static size_t __stdcall SignalEntry(const kiv_hal::TRegisters & context)
+{
+	kiv_os::NSignal_Id signal = static_cast<kiv_os::NSignal_Id>(context.rcx.l);
+
+	if (g_threadEnv.signalHandler)
+	{
+		// spuštění handleru nastaveného pomocí RTL::SetupSignals
+		g_threadEnv.signalHandler(signal);
+	}
+
+	return 0;
+}
 
 static bool SysCall(kiv_hal::TRegisters & context)
 {
@@ -18,29 +71,191 @@ static bool SysCall(kiv_hal::TRegisters & context)
 	return true;
 }
 
+static void ConfigureSignal(kiv_os::NSignal_Id signal, uint32_t signalMask)
+{
+	const uint8_t signalNumber = static_cast<uint8_t>(signal);
+	const bool isActive = (0x1 << signalNumber) & signalMask;
+
+	kiv_hal::TRegisters registers;
+	registers.rax.h = static_cast<uint8_t>(kiv_os::NOS_Service_Major::Process);
+	registers.rax.l = static_cast<uint8_t>(kiv_os::NOS_Process::Register_Signal_Handler);
+	registers.rcx.l = signalNumber;
+	registers.rdx.r = (isActive) ? reinterpret_cast<uint64_t>(SignalEntry) : 0;
+
+	SysCall(registers);
+}
+
 
 // ========================
 // ==  Procesy a vlákna  ==
 // ========================
 
+kiv_os::THandle RTL::CreateProcess(const char *program, const RTL::ProcessEnvironment & env)
+{
+	kiv_hal::TRegisters registers;
+	registers.rax.h = static_cast<uint8_t>(kiv_os::NOS_Service_Major::Process);
+	registers.rax.l = static_cast<uint8_t>(kiv_os::NOS_Process::Clone);
+	registers.rcx.l = static_cast<uint8_t>(kiv_os::NClone::Create_Process);
+	registers.rdx.r = reinterpret_cast<uint64_t>(program);
+	registers.rdi.r = reinterpret_cast<uint64_t>(env.cmdLine);
+	registers.rbx.e = (env.stdIn << 16) | env.stdOut;
+
+	if (!SysCall(registers))
+	{
+		return 0;
+	}
+
+	return registers.rax.x;
+}
+
+kiv_os::THandle RTL::CreateThread(RTL::ThreadMain mainFunc, void *param)
+{
+	ThreadEntryData *pData = new (std::nothrow) ThreadEntryData;
+	if (!pData)
+	{
+		SetLastError(kiv_os::NOS_Error::Out_Of_Memory);
+		return 0;
+	}
+
+	pData->mainFunc = mainFunc;
+	pData->param = param;
+
+	pData->process = g_threadEnv.process;
+
+	kiv_hal::TRegisters registers;
+	registers.rax.h = static_cast<uint8_t>(kiv_os::NOS_Service_Major::Process);
+	registers.rax.l = static_cast<uint8_t>(kiv_os::NOS_Process::Clone);
+	registers.rcx.l = static_cast<uint8_t>(kiv_os::NClone::Create_Thread);
+	registers.rdx.r = reinterpret_cast<uint64_t>(ThreadEntry);
+	registers.rdi.r = reinterpret_cast<uint64_t>(pData);
+
+	if (!SysCall(registers))
+	{
+		return 0;
+	}
+
+	return registers.rax.x;
+}
+
+int RTL::WaitFor(const kiv_os::THandle *handles, uint16_t count)
+{
+	kiv_hal::TRegisters registers;
+	registers.rax.h = static_cast<uint8_t>(kiv_os::NOS_Service_Major::Process);
+	registers.rax.l = static_cast<uint8_t>(kiv_os::NOS_Process::Wait_For);
+	registers.rdx.r = reinterpret_cast<uint64_t>(handles);
+	registers.rcx.x = count;
+
+	if (!SysCall(registers))
+	{
+		return -1;
+	}
+
+	return registers.rax.x;
+}
+
+int RTL::GetExitCode(kiv_os::THandle handle)
+{
+	kiv_hal::TRegisters registers;
+	registers.rax.h = static_cast<uint8_t>(kiv_os::NOS_Service_Major::Process);
+	registers.rax.l = static_cast<uint8_t>(kiv_os::NOS_Process::Read_Exit_Code);
+	registers.rdx.x = static_cast<uint16_t>(handle);
+
+	if (!SysCall(registers))
+	{
+		return 0;
+	}
+
+	return registers.rcx.x;
+}
+
+void RTL::SetupSignals(RTL::SignalHandler handler, uint32_t signalMask)
+{
+	if (handler == nullptr || signalMask == 0)
+	{
+		handler = nullptr;
+		signalMask = 0;
+	}
+
+	g_threadEnv.signalHandler = handler;
+	g_threadEnv.signalMask = signalMask;
+
+	// aktualizace nastavení všech signálů
+	ConfigureSignal(kiv_os::NSignal_Id::Terminate, signalMask);  // API definuje pouze jeden signál
+}
+
+RTL::SignalHandler RTL::GetSignalHandler()
+{
+	return g_threadEnv.signalHandler;
+}
+
+uint32_t RTL::GetSignalMask()
+{
+	return g_threadEnv.signalMask;
+}
+
 const char *RTL::GetProcessCmdLine()
 {
-	return g_threadEnv.processCmdLine;
+	return g_threadEnv.process.cmdLine;
 }
 
 kiv_os::THandle RTL::GetStdInHandle()
 {
-	return g_threadEnv.stdIn;
+	return g_threadEnv.process.stdIn;
 }
 
 kiv_os::THandle RTL::GetStdOutHandle()
 {
-	return g_threadEnv.stdOut;
+	return g_threadEnv.process.stdOut;
 }
 
-ThreadEnvironment *RTL::GetCurrentThreadEnv()
+std::string RTL::GetWorkingDirectory()
+{
+	char buffer[4096];
+
+	kiv_hal::TRegisters registers;
+	registers.rax.h = static_cast<uint8_t>(kiv_os::NOS_Service_Major::File_System);
+	registers.rax.l = static_cast<uint8_t>(kiv_os::NOS_File_System::Get_Working_Dir);
+	registers.rdx.r = reinterpret_cast<uint64_t>(buffer);
+	registers.rcx.r = sizeof buffer;
+
+	if (!SysCall(registers))
+	{
+		return std::string();
+	}
+
+	const size_t length = registers.rax.r;
+
+	return std::string(buffer, length);
+}
+
+bool RTL::SetWorkingDirectory(const char *path)
+{
+	kiv_hal::TRegisters registers;
+	registers.rax.h = static_cast<uint8_t>(kiv_os::NOS_Service_Major::File_System);
+	registers.rax.l = static_cast<uint8_t>(kiv_os::NOS_File_System::Set_Working_Dir);
+	registers.rdx.r = reinterpret_cast<uint64_t>(path);
+
+	if (!SysCall(registers))
+	{
+		return false;
+	}
+
+	return true;
+}
+
+RTL::ThreadEnvironment *RTL::GetCurrentThreadEnv()
 {
 	return &g_threadEnv;
+}
+
+void RTL::SetCurrentThreadExitCode(int exitCode)
+{
+	kiv_hal::TRegisters registers;
+	registers.rax.h = static_cast<uint8_t>(kiv_os::NOS_Service_Major::Process);
+	registers.rax.l = static_cast<uint8_t>(kiv_os::NOS_Process::Exit);
+	registers.rcx.x = static_cast<uint16_t>(exitCode);
+
+	SysCall(registers);
 }
 
 
@@ -58,10 +273,45 @@ void RTL::SetLastError(kiv_os::NOS_Error error)
 	g_threadEnv.lastError = error;
 }
 
+std::string RTL::ErrorToString(kiv_os::NOS_Error error)
+{
+	switch (error)
+	{
+		case kiv_os::NOS_Error::Success:               return "Vse v poradku";
+		case kiv_os::NOS_Error::Invalid_Argument:      return "Neplatny parametr";
+		case kiv_os::NOS_Error::File_Not_Found:        return "Soubor nenalezen";
+		case kiv_os::NOS_Error::Directory_Not_Empty:   return "Adresar neni prazdny";
+		case kiv_os::NOS_Error::Not_Enough_Disk_Space: return "Nedostatek mista na disku";
+		case kiv_os::NOS_Error::Out_Of_Memory:         return "Nedostatek pameti";
+		case kiv_os::NOS_Error::Permission_Denied:     return "Pristup odepren";
+		case kiv_os::NOS_Error::IO_Error:              return "Chyba IO";
+		case kiv_os::NOS_Error::Unknown_Error:         break;
+	}
+
+	return "Neznama chyba";
+}
+
 
 // =========================================
 // ==  Soubory a standardní vstup/výstup  ==
 // =========================================
+
+kiv_os::THandle RTL::OpenFile(const char *path, kiv_os::NOpen_File flags, kiv_os::NFile_Attributes attributes)
+{
+	kiv_hal::TRegisters registers;
+	registers.rax.h = static_cast<uint8_t>(kiv_os::NOS_Service_Major::File_System);
+	registers.rax.l = static_cast<uint8_t>(kiv_os::NOS_File_System::Open_File);
+	registers.rcx.l = static_cast<uint8_t>(flags);
+	registers.rdi.i = static_cast<uint16_t>(attributes);
+	registers.rdx.r = reinterpret_cast<uint64_t>(path);
+
+	if (!SysCall(registers))
+	{
+		return 0;
+	}
+
+	return registers.rax.x;
+}
 
 bool RTL::ReadFile(kiv_os::THandle file, char *buffer, size_t size, size_t *pRead)
 {
@@ -105,6 +355,94 @@ bool RTL::WriteFile(kiv_os::THandle file, const char *buffer, size_t size, size_
 	}
 
 	return true;
+}
+
+bool RTL::SeekFile(kiv_os::THandle file, kiv_os::NFile_Seek action, kiv_os::NFile_Seek base, int64_t & pos)
+{
+	kiv_hal::TRegisters registers;
+	registers.rax.h = static_cast<uint8_t>(kiv_os::NOS_Service_Major::File_System);
+	registers.rax.l = static_cast<uint8_t>(kiv_os::NOS_File_System::Seek);
+	registers.rcx.h = static_cast<uint8_t>(action);
+	registers.rcx.l = static_cast<uint8_t>(base);
+	registers.rdx.x = file;
+	registers.rdi.r = pos;
+
+	if (!SysCall(registers))
+	{
+		return false;
+	}
+
+	if (action == kiv_os::NFile_Seek::Get_Position)
+	{
+		pos = registers.rax.r;
+	}
+
+	return true;
+}
+
+bool RTL::DeleteFile(const char *path)
+{
+	kiv_hal::TRegisters registers;
+	registers.rax.h = static_cast<uint8_t>(kiv_os::NOS_Service_Major::File_System);
+	registers.rax.l = static_cast<uint8_t>(kiv_os::NOS_File_System::Close_Handle);
+	registers.rdx.r = reinterpret_cast<uint64_t>(path);
+
+	if (!SysCall(registers))
+	{
+		return false;
+	}
+
+	return true;
+}
+
+
+// ===============
+// ==  Ostatní  ==
+// ===============
+
+RTL::Pipe RTL::CreatePipe()
+{
+	kiv_os::THandle handles[2];
+
+	kiv_hal::TRegisters registers;
+	registers.rax.h = static_cast<uint8_t>(kiv_os::NOS_Service_Major::File_System);
+	registers.rax.l = static_cast<uint8_t>(kiv_os::NOS_File_System::Create_Pipe);
+	registers.rdx.r = reinterpret_cast<uint64_t>(handles);
+
+	if (!SysCall(registers))
+	{
+		return Pipe();
+	}
+
+	Pipe pipe;
+	pipe.readEnd  = handles[1];
+	pipe.writeEnd = handles[0];
+
+	return pipe;
+}
+
+bool RTL::CloseHandle(kiv_os::THandle handle)
+{
+	kiv_hal::TRegisters registers;
+	registers.rax.h = static_cast<uint8_t>(kiv_os::NOS_Service_Major::File_System);
+	registers.rax.l = static_cast<uint8_t>(kiv_os::NOS_File_System::Close_Handle);
+	registers.rdx.x = handle;
+
+	if (!SysCall(registers))
+	{
+		return false;
+	}
+
+	return true;
+}
+
+void RTL::Shutdown()
+{
+	kiv_hal::TRegisters registers;
+	registers.rax.h = static_cast<uint8_t>(kiv_os::NOS_Service_Major::Process);
+	registers.rax.l = static_cast<uint8_t>(kiv_os::NOS_Process::Shutdown);
+
+	SysCall(registers);
 }
 
 
--- src/user/rtl.h
+++ src/user/rtl.h
@@ -2,27 +2,21 @@
 
 #include <cstring>
 #include <string>
+#include <vector>
 
 #include "../api/api.h"
-
-struct ThreadEnvironment
-{
-	kiv_os::NOS_Error lastError = kiv_os::NOS_Error::Success;
-	kiv_os::THandle stdIn  = 0;  // nula je lepší než -1 jako invalid handle...
-	kiv_os::THandle stdOut = 0;  // ...protože "if (handle)" je mnohem lepší než "if (handle != kiv_os::Invalid_Handle)"
-	const char *processCmdLine = "";
-};
 
 // =========================================================================================
 #define RTL_DEFINE_SHELL_PROGRAM(NAME)\
 static int NAME##_main(const char *args);\
 extern "C" __declspec(dllexport) size_t __stdcall NAME(const kiv_hal::TRegisters & context)\
 {\
-	ThreadEnvironment *pEnv = RTL::GetCurrentThreadEnv();\
-	pEnv->stdIn  = static_cast<kiv_os::THandle>(context.rax.x);\
-	pEnv->stdOut = static_cast<kiv_os::THandle>(context.rbx.x);\
-	pEnv->processCmdLine = reinterpret_cast<const char*>(context.rdi.r);\
-	return NAME##_main(pEnv->processCmdLine);\
+	RTL::ThreadEnvironment *pEnv = RTL::GetCurrentThreadEnv();\
+	pEnv->process.stdIn   = static_cast<kiv_os::THandle>(context.rax.x);\
+	pEnv->process.stdOut  = static_cast<kiv_os::THandle>(context.rbx.x);\
+	pEnv->process.cmdLine = reinterpret_cast<const char*>(context.rdi.r);\
+	RTL::SetCurrentThreadExitCode(NAME##_main(RTL::GetProcessCmdLine()));\
+	return 0;\
 }
 // =========================================================================================
 
@@ -31,6 +25,107 @@ namespace RTL
 	// ========================
 	// ==  Procesy a vlákna  ==
 	// ========================
+
+	using ThreadMain = int (*)(void *param);
+	using SignalHandler = void (*)(kiv_os::NSignal_Id signal);
+
+	struct ProcessEnvironment
+	{
+		kiv_os::THandle stdIn  = 0;  // nula je lepší než -1 jako invalid handle
+		kiv_os::THandle stdOut = 0;  // protože "if (handle)" je lepší než "if (handle != kiv_os::Invalid_Handle)"
+		const char *cmdLine    = "";
+	};
+
+	struct ThreadEnvironment
+	{
+		kiv_os::NOS_Error lastError = kiv_os::NOS_Error::Success;
+		SignalHandler signalHandler = nullptr;
+		uint32_t signalMask = 0;
+
+		// informace o procesu ve kterém je vlákno spuštěno
+		ProcessEnvironment process;
+	};
+
+	/**
+	 * @brief Vytvoří nový proces.
+	 * Handle procesu by se měl uzavřít pomocí RTL::CloseHandle, pokud už není potřeba. Handle na ukončený proces je možné
+	 * použít pro získání návratového kódu pomocí RTL::GetExitCode. Při uzavření handle na ukončený proces je tento proces
+	 * odstraněn ze systému. Při uzavření handle na běžící proces je daný proces odstraněn automaticky po skončení. Všechny
+	 * ukončené procesy jsou rovněž automaticky odstraněny po skončení procesu, který je spustil. Také proces běžící i po
+	 * ukončení procesu, který jej spustil, je automaticky odstraněn po svém ukončení.
+	 * @param program Řetězec ukončený nulou s názvem programu, který se má spustit. Například "dir".
+	 * @param env Prostředí nového procesu.
+	 * @return Handle na nový proces nebo 0, pokud došlo k chybě. Chybový kód je možné získat pomocí RTL::GetLastError.
+	 */
+	kiv_os::THandle CreateProcess(const char *program, const ProcessEnvironment & env);
+
+	inline kiv_os::THandle CreateProcess(const std::string & program, const ProcessEnvironment & env)
+	{
+		return CreateProcess(program.c_str(), env);
+	}
+
+	/**
+	 * @brief Vytvoří nové vlákno v kontextu aktuálního procesu.
+	 * Handle vlákna by se měl uzavřít pomocí RTL::CloseHandle, pokud už není potřeba. Handle na ukončené vlákno je možné
+	 * použít pro získání návratového kódu pomocí RTL::GetExitCode. Při uzavření handle na ukončené vlákno je toto vlákno
+	 * odstraněno ze systému. Při uzavření handle na běžící vlákno je dané vlákno odstraněno automaticky po skončení. Při
+	 * ukončení procesu jsou také automaticky odstraněna všechna jeho vlákna. Proces se ukončí pouze tehdy, když skončí
+	 * všechna jeho vlákna.
+	 * @param mainFunc Main funkce nového vlákna.
+	 * @param param Libovolná hodnota předaná main funkci nového vlákna jako parametr.
+	 * @return Handle na nové vlákno nebo 0, pokud došlo k chybě. Chybový kód je možné získat pomocí RTL::GetLastError.
+	 */
+	kiv_os::THandle CreateThread(ThreadMain mainFunc, void *param);
+
+	/**
+	 * @brief Blokuje, dokud se některý ze zadaných procesů nebo vláken neukončí.
+	 * @param handles Pole obsahující handle procesů nebo vláken, na které se má čekat.
+	 * @param count Celkový počet procesů nebo vláken, na které se má čekat.
+	 * @return Index handle procesu nebo vlákna, který je ukončený nebo se ukončil během čekání. Pokud došlo k chybě, tak -1.
+	 * Chybový kód je možné získat pomocí RTL::GetLastError.
+	 */
+	int WaitFor(const kiv_os::THandle *handles, uint16_t count);
+
+	inline int WaitFor(const std::vector<kiv_os::THandle> handles)
+	{
+		return WaitFor(handles.data(), static_cast<uint16_t>(handles.size()));
+	}
+
+	/**
+	 * @brief Vrátí návratový kód procesu nebo vlákna.
+	 * Pokud proces nebo vlákno stále běží, tak tato funkce vrací vždy 0 s chybovým kódem kiv_os::NOS_Error::Success.
+	 * Pokud parametr handle není validní handle na existující proces nebo vlákno, tak návratový kód je také 0, ale chybový
+	 * kód se nastaví na kiv_os::NOS_Error::Invalid_Argument.
+	 * @param handle Handle na proces nebo vlákno.
+	 * @return Návratový kód procesu nebo vlákna.
+	 */
+	int GetExitCode(kiv_os::THandle handle);
+
+	/**
+	 * @brief Nastaví zpracování signálů pro aktuální vlákno.
+	 * Signal handler je vždy pouze jeden společný pro všechny signály. Spouští se vždy v kontextu vlákna, ve kterém je
+	 * nastaven. Po přijetí signálu dojde ke spuštění signal handleru při prvním vstupu daného vlákna do jádra (syscall).
+	 * Pokud je signál přijat v době, kdy se dané vlákno nachází uvnitř jádra, tak dojde ke spuštění signal handleru před
+	 * výstupem vlákna z jádra.
+	 * Pokud je jeden z parametrů této funkce nulový, tak se vynuluje i druhý, a pro všechny signály se nastaví výchozí
+	 * handler. Každé vlákno má na začátku nastaven stejný výchozí signal handler, který nic nedělá.
+	 * @param handler Signal handler.
+	 * @param signalMask Bitová maska, kde každý bit reprezentuje jeden signál. Pozice bitu odpovídá číslu signálu. Hodnota
+	 * bitu určuje, zda se pro daný signál použije nastavený handler (1) nebo výchozí handler (0).
+	 */
+	void SetupSignals(SignalHandler handler, uint32_t signalMask);
+
+	/**
+	 * @brief Vrátí signal handler aktuálního vlákna.
+	 * @return Signal handler nebo null, pokud signal handler není nastaven. Viz RTL::SetupSignals.
+	 */
+	SignalHandler GetSignalHandler();
+
+	/**
+	 * @brief Vrátí bitovou masku se signály aktivovanými pro aktuální vlákno.
+	 * @return Viz RTL::SetupSignals.
+	 */
+	uint32_t GetSignalMask();
 
 	/**
 	 * @brief Vrátí řetězec s argumenty aktuálního procesu.
@@ -52,11 +147,36 @@ namespace RTL
 	kiv_os::THandle GetStdOutHandle();
 
 	/**
+	 * @brief Vrátí pracovní adresář aktuálního procesu.
+	 * @return Řetězec s absolutní cestou k pracovnímu adresáři.
+	 */
+	std::string GetWorkingDirectory();
+
+	/**
+	 * @brief Nastaví pracovní adresář aktuálního procesu.
+	 * @param path Řetězec s absolutní nebo relativní cestou k pracovnímu adresáři.
+	 * @return Pokud vše proběhlo v pořádku, tak true, jinak false. Chybový kód je možné získat pomocí RTL::GetLastError.
+	 */
+	bool SetWorkingDirectory(const char *path);
+
+	inline bool SetWorkingDirectory(const std::string & path)
+	{
+		return SetWorkingDirectory(path.c_str());
+	}
+
+	/**
 	 * @brief Vrátí ukazatel na strukturu prostředí aktuálního vlákna.
 	 * Tato funkce by se neměla používat mimo RTL.
 	 * @return Prostředí aktuálního vlákna.
 	 */
 	ThreadEnvironment *GetCurrentThreadEnv();
+
+	/**
+	 * @brief Nastaví návratový kód aktuálního procesu nebo vlákna.
+	 * Tato funkce by se neměla používat mimo RTL.
+	 * @param exitCode Návratový kód.
+	 */
+	void SetCurrentThreadExitCode(int exitCode);
 
 	// ====================
 	// ==  Chybové kódy  ==
@@ -74,9 +194,40 @@ namespace RTL
 	 */
 	void SetLastError(kiv_os::NOS_Error error);
 
+	/**
+	 * @brief Převede chybový kód na řetězec s popisem chyby.
+	 * @param error Chybový kód.
+	 * @return Textová reprezentace chybového kódu.
+	 */
+	std::string ErrorToString(kiv_os::NOS_Error error);
+
+	/**
+	 * @brief Vrátí řetězec s popisem chybového kódu pro aktuální vlákno.
+	 * @return Textová reprezentace aktuálního chybového kódu.
+	 */
+	inline std::string GetLastErrorString()
+	{
+		return ErrorToString(GetLastError());
+	}
+
 	// =========================================
 	// ==  Soubory a standardní vstup/výstup  ==
 	// =========================================
+
+	/**
+	 * @brief Otevře nebo vytvoří soubor nebo adresář.
+	 * @param path Absolutní nebo relativní cesta k souboru nebo adresáři.
+	 * @param flags Možnosti otevření nebo vytvoření souboru nebo adresáře.
+	 * @param attributes Atributy souboru nebo adresáře.
+	 * @return Handle na otevřený soubor nebo adresář. Případně nula, pokud se otevření nebo vytvoření souboru nebo adresáře
+	 * nezdařilo. Chybový kód je možné získat pomocí RTL::GetLastError.
+	 */
+	kiv_os::THandle OpenFile(const char *path, kiv_os::NOpen_File flags, kiv_os::NFile_Attributes attributes);
+
+	inline kiv_os::THandle OpenFile(const std::string & path, kiv_os::NOpen_File flags, kiv_os::NFile_Attributes attributes)
+	{
+		return OpenFile(path.c_str(), flags, attributes);
+	}
 
 	/**
 	 * @brief Načte data ze souboru.
@@ -163,6 +314,65 @@ namespace RTL
 	{
 		return WriteFile(GetStdOutHandle(), string);
 	}
+
+	/**
+	 * @brief Změní nebo získá aktuální pozici v souboru.
+	 * Lze také použít pro zjištění nebo změnu velikosti souboru.
+	 * @param file Handle na soubor.
+	 * @param action Určuje, co se má provést (Get_Position, Set_Position, Set_Size).
+	 * @param base Určuje, od jakého místa se má vypočítat výsledná pozice nebo velikost (Beginning, Current, End).
+	 * @param pos Určuje, o kolik se má pozice nebo velikost změnit. Při Get_Position se sem uloží aktuální pozice.
+	 * @return Pokud vše proběhlo v pořádku, tak true, jinak false. Chybový kód je možné získat pomocí RTL::GetLastError.
+	 */
+	bool SeekFile(kiv_os::THandle file, kiv_os::NFile_Seek action, kiv_os::NFile_Seek base, int64_t & pos);
+
+	/**
+	 * @brief Odstraní soubor nebo prázdný adresář.
+	 * @param path Absolutní nebo relativní cesta k souboru nebo adresáři.
+	 * @return Pokud vše proběhlo v pořádku, tak true, jinak false. Chybový kód je možné získat pomocí RTL::GetLastError.
+	 */
+	bool DeleteFile(const char *path);
+
+	inline bool DeleteFile(const std::string & path)
+	{
+		return DeleteFile(path.c_str());
+	}
+
+	// ===============
+	// ==  Ostatní  ==
+	// ===============
+
+	struct Pipe
+	{
+		kiv_os::THandle readEnd  = 0;
+		kiv_os::THandle writeEnd = 0;
+
+		operator bool() const
+		{
+			return readEnd && writeEnd;
+		}
+	};
+
+	/**
+	 * @brief Vytvoří jednosměrnou rouru.
+	 * Pro odstranění roury ze systému je potřeba uzavřít oba konce pomocí RTL::CloseHandle.
+	 * @return Handle na čtecí a zapisovací konec roury, případně nulové handly, pokud se rouru nepodařilo vytvořit. Chybový
+	 * kód je možné získat pomocí RTL::GetLastError.
+	 */
+	Pipe CreatePipe();
+
+	/**
+	 * @brief Uzavře libovolný handle.
+	 * @param handle Handle k uzavření.
+	 * @return Pokud byl handle úspěšně uzavřen, tak true, jinak false. Chybový kód je možné získat pomocí RTL::GetLastError.
+	 */
+	bool CloseHandle(kiv_os::THandle handle);
+
+	/**
+	 * @brief Provede řádné ukončení celého systému.
+	 * Všem spuštěným procesům je odeslán signál Terminate.
+	 */
+	void Shutdown();
 }
 
 
